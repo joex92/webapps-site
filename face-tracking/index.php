6b7ba8aa6d4ddaeac874020d6833aaaefb2eb454
joex92 <joex92@ssh1.paris1.alwaysdata.com>
2021-08-01 18:49:29 +0200

    Updated Face-Tracking page

---
+++ face-tracking/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>JoeX!'s Face Blur</title>
+        <script src="../node_modules/graham_scan/src/graham_scan.js"></script>
+        <script src="../libraries/face-tracking-p5js/libs/clmtrackr.js"></script>
+        <script src="../libraries/face-tracking-p5js/libs/model_pca_20_svm.js"></script>
+        <script src="../libraries/face-tracking-p5js/libs/p5.js"></script>
+        <script src="../libraries/face-tracking-p5js/libs/p5.dom.js"></script>
+        <script src="../libraries/face-tracking-p5js/libs/emotion_classifier.js"></script>
+        <script src="../libraries/face-tracking-p5js/libs/emotionmodel.js"></script>
+        <script src="../libraries/face-tracking-p5js/libs/workshop-utils.js"></script>
+        <script src="sketch.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
