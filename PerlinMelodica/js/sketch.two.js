57fadd6d4c7b2319ce5b57a08ca68079345c87b8
joex92 <jamedinao@gmail.com>
2021-09-01 08:30:49 +0200

    working on PerlinMelodica and added Bootstrap library for later testing

--- PerlinMelodica/js/sketch.two.js
+++ PerlinMelodica/js/sketch.two.js
@@ -1,10 +1,10 @@
-var innerMin = Math.min(innerWidth,innerHeight), bg, fg, params, two, seed = Date.now(), precision = 0.1, noiseSel, noiseOpts;
+var millis = 0, fps = 0, avfps, innerMin = Math.min(innerWidth,innerHeight), bg, fg, params, two, cnv, seed = Date.now(), precision = 0.1, noiseSel, noiseOpts, noise;
 function twoSketch() {
   bg = document.getElementById('bg');
   fg = document.getElementById('fg');
   window.onmousemove = (e) => {
-    fg.style["left"] = e.clientX + "px";
-    fg.style["top"] = e.clientY + "px";
+    fg.style["left"] = (e.clientX.clip(innerWidth-fg.clientWidth)) + "px";
+    fg.style["top"] = (e.clientY.clip(innerHeight-fg.clientHeight)) + "px";
   };
   // console.log(P5N,OSN,SN,LNP);
   noiseOpts = [
@@ -16,7 +16,7 @@ function twoSketch() {
       ,
       "WasmNoise"
       ];
-  noiseSel = 0;
+  noiseSel = 3;
 
   bpm2ms = (bpm=128) => {
       return (60000)/bpm;
@@ -33,39 +33,33 @@ function twoSketch() {
       //   LNP.SetSeed(s);
   };
 
-  genNoise = (img) => {
-      // if (img.loadPixels()) {
-      //     img.loadPixels();
-      //     for (let i = 0; i < img.pixels.length/4; i++){
-      //         img.pixels[(i*4)+3] = 255;
-      //     }
-      //     for (let x = 1; x < img.width-1; x++) {
-      //         for (let y = 1; y < img.height-1; y++) {
-      //             // Calculate the 1D location from a 2D grid
-      //             let rgb = wasm.map(getNoiseVal(x,y,z),-1,1,0,255), loc = ((x + y * img.width) * pd);
-      //             // arrgraph.push([rgb,loc]);
-      //             img.pixels[(loc + 0)] = rgb;
-      //             img.pixels[(loc + 1)] = rgb;
-      //             img.pixels[(loc + 2)] = rgb;
-      //             img.pixels[(loc + 4)] = 0.5;
-      //         }
-      //     }
-      //     img.updatePixels();
-      // } else {
-      //     arrgraph = [];
-      //     for (let x = 1; x < img.width-1; x++) {
-      //         for (let y = 1; y < img.height-1; y++) {
-      //             let rgb = wasm.map(getNoiseVal(x,y,z),-1,1,0,255);
-      //             arrgraph.push([rgb,x-img.width/2,y-img.height/2,1,1]);
-      //             // img.stroke(rgb);
-      //             // img.strokeWeight(pd);
-      //             // img.point(x-img.width/2,y-img.height/2,0);
-      //             img.fill(rgb);
-      //             img.noStroke();
-      //             img.rect(x-img.width/2,y-img.height/2,1,1);
-      //         }
-      //     }
-      // }
+  genNoise = (twoObj, nw = innerWidth, nh = innerHeight) => {
+    for (let x = 0; x < nw; x++){
+      for (let y = 0; y < nh; y++){
+        const val = getNoiseVal(x,y,0);
+        switch (objType(twoObj)) {
+          case 'Group':
+            let p = new Two.Rectangle(x+0.5,y+0.5,1,1);
+            p.fill = "rgba(255,255,255,"+val+")";
+            twoObj.add(p);
+            break;
+          case 'Points':
+            break;
+          case 'ImageData':
+            let i = ((x + y * twoObj.width) * 4);
+            twoObj.data[i+0] = 255;
+            twoObj.data[i+1] = 255;
+            twoObj.data[i+2] = 255;
+            twoObj.data[i+3] = val.lmap(-1,1,0,255);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    if (objType(twoObj) === 'ImageData'){
+      cnv.putImageData(twoObj, 0, 0);
+    }
   };
 
   getNoiseVal = (x,y,z) => {
@@ -93,17 +87,42 @@ function twoSketch() {
   setNoiseSeed(seed);
   // Make an instance of two and place it on the page.
   params = {
-    type: Two.Types.svg,
+    type: Two.Types.canvas,
     width: innerWidth,
     height: innerHeight };
   two = new Two(params).appendTo(bg);
-  two.renderer.domElement.style["background-color"] = "#000000"; // background;
+  two.renderer.domElement.style["background-color"] = "#000000ff"; // background;
+  switch (two.type) {
+    case Two.Types.canvas:
+      cnv = two.renderer.domElement.getContext('2d');
+      break;
+    case Two.Types.webgl:
+      cnv = two.renderer.domElement.getContext('webgl');
+      break;
+    default:
+      cnv = two.renderer.domElement;
+      break;
+  }
   window.onresize = (e) => {
     console.log(e);
     two.width = innerWidth;
     two.height = innerHeight;
     innerMin = Math.min(innerWidth,innerHeight);
   };
+  noise = two.makeGroup();
+  noise.noStroke();
+  noise.scale = 1;
+
+  twoDraw = (frameCount,timeDelta) => {
+    millis += timeDelta;
+    avfps = 1000*(frameCount/millis);
+    fps = 1000/timeDelta;
+    fg.innerHTML = "<br><b>Frame Count:</b> <i>"+frameCount+"</i>f<br><b>Delta Time:</b> <i>"+timeDelta+"</i>ms<br><b>Frame Rate:</b> <i>"+fps.toFixed(2)+"</i>fps<br><b>Average Frame Rate:</b> <i>"+avfps.toFixed(2)+"</i>fps";
+  };
+
+  // Bind a function to scale and rotate the group
+  // to the animation loop.
+  two.bind('update', twoDraw).play();  // Finally, start the animation loop
 }
 
 var libNoiseScript = loadJSscript("https://"+location.host+"/libraries/joex-p5noise-libnoise-helper.js",(e) => {
@@ -111,13 +130,13 @@ var libNoiseScript = loadJSscript("https://"+location.host+"/libraries/joex-p5no
   LNP = new PerlinNoise(seed);
   // import("https://"+location.host+"/node_modules/wasm-noise/wasm_noise.js").then((rs0,rj0) => {
   //   if (rs0) {
-  //     console.log("Response:\n",rs0);
+  //     console.log("Response:\n",rs0,rs0.toString());
       import("https://unpkg.com/tone").then((rs1,rj1) => {
         if (rs1) {
-          console.log("Response:\n",rs1);
+          console.log("Response:\n",rs1,propsAsString(rs1,"\n"));
           import("https://"+location.host+"/libraries/two.js/build/two.js").then((rs2,rj2) => {
             if (rs2) {
-              console.log("Response:\n",rs2);
+              console.log("Response:\n",rs2,propsAsString(rs2,"\n"));
               WasmNoise.onLoaded = () => {
                 twoSketch();
               };
--- libraries/joex-utils.js
+++ libraries/joex-utils.js
@@ -158,3 +158,401 @@ function loadJSscript(url, callback=null, doc=document){
     doc.getElementsByTagName("head")[0].appendChild(script);
     return script;
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const __ = undefined,
+      TWO_PI = Math.PI*2,
+      G = 6.67430*Math.pow(10,-11), // m/s2
+      EARTH_M = 5.972*Math.pow(10,24), // kg
+      EARTH_R = 6371000, // kg
+      EVEREST_PEAK = 6382300, // m
+      CHIMBORAZO_PEAK = 6384400, // m
+      C_VACUUM = 299792458, // Lightspeed on Vacuum
+      C_AIR = C_VACUUM/1.0003, // Lightspeed on Air
+      C_WATER = C_VACUUM/1.33, // Lightspeed on Water
+      NM = Math.pow(10,9), // 1 meter to nanometers
+      TO40OCT = Math.pow(2,40); // freq to 40 octaves higher
+var gx = 0, gy = planetGrav();
+var curl = document.URL;
+var or = {};
+
+function sleep(miliseconds) {
+  var currentTime = new Date().getTime();
+  while (currentTime + miliseconds >= new Date().getTime()){}
+}
+
+function planetGrav(mass=EARTH_M,atRadius=EARTH_R){
+  return G*(mass/Math.pow(atRadius,2));
+}
+
+function copyObj(obj){
+  return JSON.parse(JSON.stringify(obj));
+}
+
+function propsAsString(obj={},sep=" ") {
+  return Object.keys(obj).map(function (k) {
+    return k + ": " + obj[k];
+  }).join(sep);
+}
+
+//   Object.prototype.isObjType = function (obj = "Object"){
+//       return this.valueOf().objType() == obj;
+//   }
+
+function objType(obj) {
+  if (obj != (undefined || null)) {
+    return obj.constructor.toString().split(" ")[1].split("(")[0];
+  } else {
+    return null;
+  }
+}
+
+function isNumber(obj) {
+  if (obj != (undefined || null)) {
+    if(objType(obj)=="Number"){
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+Number.prototype.lmap = function (fromMin,fromMax,toMin,toMax) {
+  let inmin, inmax, outmin, outmax;
+  switch(arguments.length){
+    case 0:
+      inmin = 0;
+      inmax = 1;
+      outmin = -1;
+      outmax = 1;
+      break;
+    case 1:
+      inmin = 0;
+      inmax = 1;
+      outmin = 0;
+      outmax = arguments[0];
+      break;
+    case 2:
+      inmin = 0;
+      inmax = 1;
+      outmin = arguments[0];
+      outmax = arguments[1];
+      break;
+    case 3:
+      inmin = arguments[0];
+      inmax = arguments[1];
+      outmin = 0;
+      outmax = arguments[2];
+      break;
+    case 4:
+      inmin = arguments[0];
+      inmax = arguments[1];
+      outmin = arguments[2];
+      outmax = arguments[3];
+      break;
+    default:
+      inmin = arguments[0];
+      inmax = arguments[1];
+      outmin = arguments[2];
+      outmax = arguments[3];
+      break;
+  }
+  if (inmin!=inmax){
+    const R = (outmax-outmin)/(inmax-inmin);
+    return (this.valueOf()-inmin)*R+outmin;
+  } else{
+    return this.valueOf();
+  }
+};
+
+Number.prototype.clip = function (mn=-1,mx=1) {
+  if(arguments.length==1||arguments.length==2){
+    if (arguments.length==1){
+      mn = -Infinity;
+      mx = arguments[0];
+    } else{
+      mn = arguments[0];
+      mx = arguments[1];
+    }
+  }
+  return Math.min(mx, Math.max(this.valueOf(), mn));
+};
+
+Number.prototype.toFixed = function (fixed = 1) {
+  let decimals = Math.pow(10, fixed);
+  return Math.round(this.valueOf() * decimals) / decimals;
+};
+
+function toFixed(obj, fixed) {
+  if (objType(obj) == "Number") {
+    return obj.toFixed(fixed);
+  } else {
+    return obj;
+  }
+}
+
+function copyToClipboard(content) {
+    var dummy = document.createElement("textarea");
+    // to avoid breaking orgain page when copying more words
+    // cant copy when adding below this code
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+    dummy.value = content;
+    dummy.select();
+    dummy.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+
+//   function random(min,max){
+//     if(objType(min)=="Number" && objType(max)!="Number"){
+//       max = min;
+//       min = 0;
+//     }
+//     if(objType(max)=="Number" && objType(min)!="Number"){
+//       min = 0;
+//     }
+//     if (objType(max)!="Number" && objType(max)!="Number"){
+//       if(min!=undefined||max!=undefined){console.log("Invalid Random Values");}
+//       return Math.random();
+//     }else{
+//       if (min>max){
+//         return (Math.random()*(min-max))+max;
+//       } else {
+//         return (Math.random()*(max-min))+min;
+//       }
+//     }
+//   }
+
+//   window.addEventListener("hover", function (event) {
+//       console.log(event);
+//   });
+
+//   window.addEventListener("mouseout", function (event) {
+//     //   console.log(event);
+//   });
+
+window.addEventListener("deviceorientation",
+//   window._ondeviceorientation =
+  (event) => {
+    //   console.log(event);
+    or = {
+      absolute: toFixed(event.absolute),
+      alpha: toFixed(event.alpha),
+      beta: toFixed(event.beta),
+      gamma: toFixed(event.gamma),
+      webkitCompassHeading: toFixed(event.webkitCompassHeading),
+      webkitCompassAccuracy: toFixed(event.webkitCompassAccuracy),
+    };
+  }
+);
+
+//   function logx(value,base){
+//     let b,v;
+//     if(arguments.length==2){
+//       v = arguments[0];
+//       b = arguments[1];
+//       if (isNumber(v)&&isNumber(b)){
+//         return Math.log10(v)/Math.log10(b);
+//       } else{
+//         return null;
+//       }
+//     } else {
+//       return null;
+//     }
+//   }
+
+function ms2ftime(ms, ly = 0) {
+  if (objType(ms)=="Number") {
+    let s = ms / 1000.0;
+    let m = (s - (s % 1)) / 60.0;
+    let h = m / 60.0;
+    let d = h / 24.0;
+    let y = d / 365.0;
+    return [
+      Math.floor(y),
+      Math.floor(d) % (365 + ly),
+      Math.floor(h) % 24,
+      Math.floor(m) % 60,
+      s % 60,
+    ];
+  } else {
+    return [0, 0, 0, 0];
+  }
+}
+
+function vf(p0,p1,dt=1){
+  // console.log(`${propsAsString(p0).join("\n")}\n${propsAsString(p1).join("\n")}\n${dt}\n`);
+  let dx = p1.x-p0.x;
+  let dy = p1.y-p0.y;
+  // print(dx+"\n"+dy)
+  // let d = Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
+  let vx = dx/dt;
+  let vy = (dy-0.5*9.8*Math.pow(dt,2))/dt;
+  let v = Math.sqrt(Math.pow(vx,2)+Math.pow(vy,2));
+  t = Math.atan2(dy,dx);
+  return {vel:v,ang:t};
+}
+
+function pol2car(pol={vel:0,ang:TWO_PI}){
+  let car = {
+      x:pol.vel*Math.cos(pol.ang),
+      y:pol.vel*Math.sin(pol.ang)
+  };
+  return car;
+}
+
+function rgba2hex(orig) {
+    var a,
+      rgb = orig
+        .replace(/\s/g, "")
+        .match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i),
+      alpha = ((rgb && rgb[4]) || "").trim(),
+      hex = rgb
+        ? (Math.round(rgb[1]) | (1 << 8)).toString(16).slice(1) +
+          (Math.round(rgb[2]) | (1 << 8)).toString(16).slice(1) +
+          (Math.round(rgb[3]) | (1 << 8)).toString(16).slice(1)
+        : orig;
+
+    if (alpha !== "") {
+      a = alpha;
+    } else {
+      a = 01;
+    }
+    // multiply before convert to HEX
+    a = ((a * 255) | (1 << 8)).toString(16).slice(1);
+    // hex = hex + a;
+
+    return "#" + hex;
+}
+
+function rgb2rgba(src, a = 1) {
+    if (src != undefined && objType(src) == "Array") {
+      let is255,
+        isFloat,
+        rgb = Array.from(src);
+      for (v of rgb) {
+        // console.log(v);
+        is255 = false;
+        if (v > 1) {
+          is255 = true;
+        }
+        isFloat = false;
+        if (v % 1 > 0) {
+          isFloat = true;
+        }
+      }
+      // console.log(`255 = ${is255}`);
+      // console.log(`Float = ${isFloat}`);
+      if (!is255 && isFloat) {
+        rgb.forEach((v, i, a) => {
+          a[i] = (v * 255);
+        });
+      }
+      return "rgba("+Math.round(rgb[0])+","+Math.round(rgb[1])+","+Math.round(rgb[2])+","+a+")";
+    } else {
+      return "rgba(0,0,0,0)";
+    }
+}
+
+function freq2wavelength(freq=440,c=C_VACUUM){
+    return (c/freq)*NM; // Wavelength in nanometers
+}
+
+function wavelength2freq(wavelength=freq2wavelength(440*TO40OCT),c=C_VACUUM){
+    wl = wavelength/NM;
+    return (c/wl);
+}
+
+// takes wavelength in nm and returns an rgba value
+function wavelength2color(wavelength) {
+    var R,
+        G,
+        B,
+        alpha,
+        colorSpace,
+        wl = wavelength
+        // ,gamma = 1
+        ;
+
+    if (wl >= 380 && wl < 440) {
+        R = -1 * (wl - 440) / (440 - 380);
+        G = 0;
+        B = 1;
+   } else if (wl >= 440 && wl < 490) {
+        R = 0;
+        G = (wl - 440) / (490 - 440);
+        B = 1;
+    } else if (wl >= 490 && wl < 510) {
+        R = 0;
+        G = 1;
+        B = -1 * (wl - 510) / (510 - 490);
+    } else if (wl >= 510 && wl < 580) {
+        R = (wl - 510) / (580 - 510);
+        G = 1;
+        B = 0;
+    } else if (wl >= 580 && wl < 760) {
+        R = 1;
+        G = -1 * (wl - 760) / (760 - 580);
+        B = 0;
+    // } else if (wl >= 580 && wl < 645) {
+    //     R = 1;
+    //     G = -1 * (wl - 645) / (645 - 580);
+    //     B = 0;
+    // } else if (wl >= 645 && wl <= 760) { // Changed 780 to 760
+    //     R = 1;
+    //     G = 0;
+    //     B = 0;
+    } else {
+        R = 0;
+        G = 0;
+        B = 0;
+    }
+
+    // intensty is lower at the edges of the visible spectrum.
+    if (wl > 780 || wl < 380) {
+        alpha = 0;
+    } else if (wl > 700) {
+        alpha = (780 - wl) / (780 - 700);
+    } else if (wl < 420) {
+        alpha = (wl - 380) / (420 - 380);
+    } else {
+        alpha = 1;
+    }
+
+    colorSpace = ["rgba(" + Math.round(R * 255) + "," + Math.round(G * 255) + "," + Math.round(B * 255) + ", " + alpha + ")", R, G, B, alpha]
+
+    // colorSpace is an array with 5 elements.
+    // The first element is the complete code as a string.
+    // Use colorSpace[0] as is to display the desired color.
+    // use the last four elements alone or together to access each of the individual r, g, b and a channels.
+
+    return colorSpace;
+
+}
+
+function freq2color(freq=440){
+    return wavelength2color(freq2wavelength(freq*TO40OCT));
+}
+
+function midi2color (midi=69) {
+    let f,d;
+    if (midi >= 66 && midi <= 77){
+        d = 0;
+    } else if (midi < 66) {
+        d = -(Math.ceil((66-midi)/12))
+    } else if (midi > 77) {
+        d = Math.ceil((midi-77)/12)
+    }
+    f = midiToFreq(midi-(12*d));
+    let rgba = freq2color(f)[0]
+    let hue = chroma(rgba).hsl()[0];
+    let light = Math.map(d,-5,5,0,1);
+    let hsl = [hue,1,light];
+    console.log(d,f,rgba,hsl);
+    return chroma(hsl,"hsl");
+}
